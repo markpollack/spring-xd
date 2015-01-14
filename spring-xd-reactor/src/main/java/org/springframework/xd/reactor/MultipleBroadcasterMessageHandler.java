358a94a79ef3598a960887fd463d3a80ef148f0e
Mark Pollack <mpollack@pivotal.io>
2015-01-14 17:39:18 -0500

    XD-2570 Code cleanup

--- spring-xd-reactor/src/main/java/org/springframework/xd/reactor/MultipleBroadcasterMessageHandler.java
+++ spring-xd-reactor/src/main/java/org/springframework/xd/reactor/MultipleBroadcasterMessageHandler.java
@@ -19,6 +19,11 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.core.ResolvableType;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.integration.expression.IntegrationEvaluationContextAware;
 import org.springframework.integration.handler.AbstractMessageProducingHandler;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandlingException;
@@ -40,30 +45,48 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * A handler that adapts item at a time delivery in a
- * {@link org.springframework.messaging.MessageHandler}
- * and delegates processing to a Reactor Stream with synchronous dispatch  so that processing
- * occurs on the same thread that invokes the handler.
+ * Adapts the item at a time delivery of a {@link org.springframework.messaging.MessageHandler}
+ * by delegating processing to an Stream based on a partitionExpression.
  * <p/>
- * The output stream of the processor is consumed and sent to the output channel.
+ * The specific Stream that the message is delegated is determined by the partitionExpression value.
+ * Unless you change the scheduling of the inputStream in your processor, you should ensure that the
+ * partitionExpression does not map messages delivered on different message bus dispatcher threads to the same
+ * stream. This is due to the underlying use of a <code>Broadcaster</code>.
+ * <p/>
+ * For example, using the expression <code>T(java.lang.Thread).currentThread().getId()</code> would map the current
+ * dispatcher thread id to an instance of a Stream. If you wanted to have a Stream per
+ * Kafka partition, you can use the expression <code>header['kafka_partition_id']</code> since the MessageBus
+ * dispatcher thread will be the same for each partition.
+ * <p/>
+ * If the Stream mapped to the partitionExpression value has an error or completes, it will be recreated when the
+ * next message consumed maps to the same partitionExpression value.
+ * <p/>
+ * All error handling is the responsibility of the processor implementation.
  *
  * @author Mark Pollack
  */
-public class SynchronousDispatcherMessageHandler extends AbstractMessageProducingHandler implements DisposableBean {
+public class MultipleBroadcasterMessageHandler extends AbstractMessageProducingHandler implements DisposableBean,
+        IntegrationEvaluationContextAware {
 
     protected final Log logger = LogFactory.getLog(getClass());
 
-    private final ConcurrentMap<Long, Broadcaster<Object>> broadcasterMap =
-            new ConcurrentHashMap<Long, Broadcaster<Object>>();
+    private final ConcurrentMap<Object, Broadcaster<Object>> broadcasterMap =
+            new ConcurrentHashMap<Object, Broadcaster<Object>>();
 
-    private final Map<Long, Controls> controlsMap = new Hashtable<Long, Controls>();
+    private final Map<Object, Controls> controlsMap = new Hashtable<Object, Controls>();
 
     private final Environment environment;
 
     @SuppressWarnings("rawtypes")
-    private final Processor reactorProcessor;
+    private final Processor processor;
 
     private final ResolvableType inputType;
+
+    private final Expression partitionExpression;
+
+    private final SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
+
+    private EvaluationContext evaluationContext = new StandardEvaluationContext();
 
     /**
      * Construct a new SynchronousDispatcherMessageHandler given the reactor based Processor to delegate
@@ -72,17 +95,19 @@ public class SynchronousDispatcherMessageHandler extends AbstractMessageProducin
      * @param processor The stream based reactor processor
      */
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public SynchronousDispatcherMessageHandler(Processor processor) {
+    public MultipleBroadcasterMessageHandler(Processor processor, String partitionExpression) {
         Assert.notNull(processor, "processor cannot be null.");
-        this.reactorProcessor = processor;
+        Assert.notNull(partitionExpression, "Partition expression can not be null");
+        this.processor = processor;
+        this.partitionExpression = spelExpressionParser.parseExpression(partitionExpression);
         environment = Environment.initializeIfEmpty(); // This by default uses SynchronousDispatcher
-        Method method = ReflectionUtils.findMethod(this.reactorProcessor.getClass(), "process", Stream.class);
+        Method method = ReflectionUtils.findMethod(this.processor.getClass(), "process", Stream.class);
         this.inputType = ResolvableType.forMethodParameter(method, 0).getNested(2);
     }
 
     @Override
     protected void handleMessageInternal(Message<?> message) {
-        Broadcaster<Object> broadcasterToUse = getBroadcaster();
+        Broadcaster<Object> broadcasterToUse = getBroadcaster(message);
         if (ClassUtils.isAssignable(inputType.getRawClass(), message.getClass())) {
             broadcasterToUse.onNext(message);
         } else if (ClassUtils.isAssignable(inputType.getRawClass(), message.getPayload().getClass())) {
@@ -101,25 +126,20 @@ public class SynchronousDispatcherMessageHandler extends AbstractMessageProducin
     }
 
 
-    private Broadcaster<Object> getBroadcaster() {
-        long idToUse = Thread.currentThread().getId();
+    private Broadcaster<Object> getBroadcaster(Message<?> message) {
+        final Object idToUse = partitionExpression.getValue(evaluationContext, message, Object.class);
+        if (logger.isDebugEnabled()) {
+            logger.debug("Partition Expression evaluated to " + idToUse);
+        }
         Broadcaster<Object> broadcaster = broadcasterMap.get(idToUse);
-
         if (broadcaster == null) {
             Broadcaster<Object> existingBroadcaster = broadcasterMap.putIfAbsent(idToUse, Streams.broadcast());
             if (existingBroadcaster == null) {
                 broadcaster = broadcasterMap.get(idToUse);
                 //user defined stream processing
-                Stream<?> outputStream = reactorProcessor.process(broadcaster);
-                //Simple log error handling
-                outputStream.when(Throwable.class, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) {
-                        logger.error(throwable);
-                    }
-                });
+                Stream<?> outputStream = processor.process(broadcaster);
 
-                Controls controls = outputStream.consume(new Consumer<Object>() {
+                final Controls controls = outputStream.consume(new Consumer<Object>() {
                     @Override
                     public void accept(Object outputObject) {
                         if (ClassUtils.isAssignable(Message.class, outputObject.getClass())) {
@@ -130,18 +150,32 @@ public class SynchronousDispatcherMessageHandler extends AbstractMessageProducin
                     }
                 });
 
-                controlsMap.put(idToUse, controls);
+                outputStream.when(Throwable.class, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) {
+                        logger.error(throwable);
+                        broadcasterMap.remove(idToUse);
+                    }
+                });
 
                 broadcaster.observeComplete(new Consumer<Void>() {
                     @Override
                     public void accept(Void aVoid) {
-                        broadcasterMap.remove(Thread.currentThread().getId());
+                        logger.error("Consumer completed for [" + controls + "]");
+                        broadcasterMap.remove(idToUse);
                     }
                 });
+
+
+                controlsMap.put(idToUse, controls);
+
+
 
                 if (logger.isDebugEnabled()) {
                     logger.debug(controls.debug());
                 }
+            } else {
+                broadcaster = existingBroadcaster;
             }
         }
         return broadcaster;
@@ -153,5 +187,10 @@ public class SynchronousDispatcherMessageHandler extends AbstractMessageProducin
             controls.cancel();
         }
         environment.shutdown();
+    }
+
+    @Override
+    public void setIntegrationEvaluationContext(EvaluationContext evaluationContext) {
+        this.evaluationContext = evaluationContext;
     }
 }
